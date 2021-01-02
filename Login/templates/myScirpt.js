2182846815a2016b606f612bd1ae8016f2b287dc
czb-1213 <59185819+czb-1213@users.noreply.github.com>
2021-01-02 12:59:10 +0800

    前端加入

---
+++ Login/templates/myScirpt.js
@@ -0,0 +1,32 @@
+// window.onload = function () {
+//     var u = document.getElementById('username');
+//     var p = document.getElementById('password');
+//     u.onblur = function () {
+//         if (u.value == '') {
+//             u.value = '账号 　 ';
+//             u.style.color = '#bbb';
+//         }
+//     }
+//     u.onfocus = function () {
+//         if (u.value == '账号 　 ') {
+//             u.value = '';
+//             u.style.color = '#000';
+//         }
+//     }
+//     p.onblur = function () {
+//         if (p.value == '') {
+//             p.type = 'text';
+//             p.value = '密码 　 ';
+//             p.style.color = '#bbb';
+//         }
+//     }
+//     p.onfocus = function () {
+//         if (p.value == '密码 　 ') {
+//             p.type = 'password';
+//             p.value = '';
+//             p.style.color = '#000';
+//         }
+//     }
+//     u.onblur();
+//     p.onblur();
+// }
